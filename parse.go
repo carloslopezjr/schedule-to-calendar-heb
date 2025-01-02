362a2357c1558faa54f4ab36e21893d77174133a
bravodude <85579815+bravodude@users.noreply.github.com>
2025-01-02 12:33:26 -0600

    return slice of mixed types

--- parse.go
+++ parse.go
@@ -10,25 +10,27 @@ import (
 	"bufio"
 )
 
-func main() {
+// create a struct that will be created each time 
+type Event struct {
+	Number string
+	Day string
+	Start_hour string
+	Start_minute string
+	S_AM_PM bool // false means am, true means pm
+	End_hour string
+	End_minute string
+	E_AM_PM bool // false means am, true means pm
+}
+
+
+func parse(path string) []interface{} {
 	// Read the entire file into memory
-	data, err := os.ReadFile("/Users/carlos/Desktop/schedule-to-calendar-heb/imessage_data/54694.txt")
+	data, err := os.ReadFile(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// create a struct that will be created each time 
-	type Event struct {
-		Number string
-		Day string
-		Start_hour string
-		Start_minute string
-		S_AM_PM bool // false means am, true means pm
-		End_hour string
-		End_minute string
-		E_AM_PM bool // false means am, true means pm
-	}
-
+	var dates []interface{}
 
 	// Create a scanner to read the file line by line
 	scanner := bufio.NewScanner(strings.NewReader(string(data)))
@@ -65,7 +67,7 @@ func main() {
 				End_minute: line[14:16],
 				E_AM_PM: E_AM_PM_bool,
 			}
-			fmt.Println(mon)
+			dates = append(dates, mon)
 		}
 		// Apply your filtering logic here
 		if strings.Contains(line, "TU") && !strings.Contains(line, "OFF"){
@@ -94,7 +96,7 @@ func main() {
 				End_minute: line[14:16],
 				E_AM_PM: E_AM_PM_bool,
 			}
-			fmt.Println(tue)
+			dates = append(dates, tue)
 		}
 		// Apply your filtering logic here
 		if strings.Contains(line, "WE") && !strings.Contains(line, "OFF"){
@@ -123,7 +125,7 @@ func main() {
 				End_minute: line[14:16],
 				E_AM_PM: E_AM_PM_bool,
 			}
-			fmt.Println(wed)
+			dates = append(dates, wed)
 		}
 		// Apply your filtering logic here
 		if strings.Contains(line, "TH") && !strings.Contains(line, "OFF"){
@@ -152,7 +154,7 @@ func main() {
 				End_minute: line[14:16],
 				E_AM_PM: E_AM_PM_bool,
 			}
-			fmt.Println(thu)
+			dates = append(dates, thu)
 		}
 		// Apply your filtering logic here
 		if strings.Contains(line, "FR") && !strings.Contains(line, "OFF"){
@@ -181,7 +183,7 @@ func main() {
 				End_minute: line[14:16],
 				E_AM_PM: E_AM_PM_bool,
 			}
-			fmt.Println(fri)
+			dates = append(dates, fri)
 		}
 		// Apply your filtering logic here
 		if strings.Contains(line, "SA") && !strings.Contains(line, "OFF"){
@@ -210,7 +212,7 @@ func main() {
 				End_minute: line[14:16],
 				E_AM_PM: E_AM_PM_bool,
 			}
-			fmt.Println(sat)
+			dates = append(dates, sat)
 		}
 		// Apply your filtering logic here
 		if strings.Contains(line, "SU") && !strings.Contains(line, "OFF"){
@@ -239,11 +241,12 @@ func main() {
 				End_minute: line[14:16],
 				E_AM_PM: E_AM_PM_bool,
 			}
-			fmt.Println(sun)
+			dates = append(dates, sun)
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error scanning file:", err)
 	}
+	return dates
 }
