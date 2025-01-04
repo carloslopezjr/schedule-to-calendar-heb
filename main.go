b91f239c3246750d491d7452c56b49a769e336bf
bravodude <85579815+bravodude@users.noreply.github.com>
2025-01-03 18:58:45 -0600

    clean up

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
-import (
-	//"fmt"
-)
-
-
 func main() {
 
 	// run parsing process
 	filepath := "/Users/carlos/Desktop/schedule-to-calendar-heb/imessage_data/54694.txt" // change to desired txt path
 	data := parse(filepath)
-
-	//cfmt.Println(data) // debugging purposes
 
 	// run google calendar process
 	run_calendar(data)
